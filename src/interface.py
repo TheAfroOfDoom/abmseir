6b8f12d7c1d8640d8fb11f0e3d25f479cc0d2822
aidant19 <aidant19@gmail.com>
2021-02-20 19:31:01 -0500

    Extended GUI

--- src/interface.py
+++ src/interface.py
@@ -7,7 +7,7 @@
 # Modified By: Aidan Tokarski
 ###
 
-import simulation as sim
+from simulation import Simulation
 import graph_handler
 import matplotlib.pyplot as plt
 import tkinter as tk
@@ -15,19 +15,22 @@ import tkinter.messagebox as tkmb
 
 class Interface:
     def __init__(self):
-        self.pop_size = 500
-        self.g = None
+        self.window = tk.Tk()
+        self.params = {}
+        '''
+        Currently implemented params:
+        pop_size
+        time_horizon
+        initial_infected_count
+        '''
 
     def simulate(self):
-        if self.g is None:
-            self.loadGraph(self.pop_size)
-        simulation = sim.Simulation(self.g)
+        sim = self.generate_sim()
         time_steps = []
-        for i in range(100):
+        for i in range(self.params['time_horizon']):
             time_steps.append(i)
-            simulation.run_step()
-        time_steps.append(time_steps[-1] + 1)
-        data = simulation.data
+            sim.run_step()
+        data = sim.data
         fig, ax = plt.subplots(1, figsize=(8, 6))
         ax.plot(time_steps, data['susceptible'], color='green', label='sus')
         ax.plot(time_steps, data['exposed'], color='orange', label='exp')
@@ -36,28 +39,43 @@ class Interface:
         ax.plot(time_steps, data['recovered'], color='blue', label='rec')
         ax.plot(time_steps, data['dead'], color='brown', label='dead')
         plt.show()
+
+    def generate_sim(self):
+        sim = Simulation(self.load_graph(int(self.window.getvar('pop_size'))))
+        for param in self.params:
+            self.params[param] = int(self.window.getvar(param))
+            if param == 'initial_infected_count':
+                sim.initial_infected_count = self.params[param]
+            elif param == 'exgogenous_rate':
+                sim.exogenous_rate = self.params[param]
+        return sim
         
-    def loadGraph(self, size):
-        self.g = graph_handler.complete_graph([size])
+    def load_graph(self, size):
+        return graph_handler.complete_graph([size])
 
-    def create_window(self):
-        window = tk.Tk()
-        self.window = window
-
-        pop_size = tk.IntVar(window, value=500, name='pop_size')
-        frm_ps = tk.Frame(window)
-        ent_pop_size = tk.Entry(frm_ps, bd=5, textvariable=pop_size)
-        ent_pop_size.pack(side=tk.RIGHT)
-        lbl_pop_size = tk.Label(frm_ps, text='Pop Size')
-        lbl_pop_size.pack(side=tk.LEFT)
-        frm_ps.pack(anchor=tk.N)
-        btn_sim = tk.Button(window, text='Simulate', command=self.btn_sim_cb)
-        btn_sim.pack(side=tk.BOTTOM)
-        window.mainloop()
+    def add_param(self, name, text, default=None):
+        # Declares a new param to be added to the interface
+        var = tk.IntVar(self.window, value=default, name=name)
+        frame = tk.Frame(self.window)
+        entry = tk.Entry(frame, bd=5, textvariable=var)
+        entry.pack(side=tk.RIGHT)
+        label = tk.Label(frame, text=text)
+        label.pack(side=tk.LEFT)
+        frame.pack(anchor=tk.N)
+        self.params[name] = default
     
     def btn_sim_cb(self):
-        self.pop_size = int(self.window.getvar('pop_size'))
         self.simulate()
+
+    def create_window(self):
+        win = self.window
+        self.add_param('pop_size', 'Pop Size', default=500)
+        self.add_param('time_horizon', 'Days', default=100)
+        self.add_param('initial_infected_count', 'Initial Infection Count', default=10)
+        self.add_param('exogenous_rate', 'Weekly Exogenous Infections', default=10)
+        btn_sim = tk.Button(win, text='Simulate', command=self.btn_sim_cb)
+        btn_sim.pack(side=tk.BOTTOM)
+        win.mainloop()
 
 if __name__ == '__main__':
     interface = Interface()
