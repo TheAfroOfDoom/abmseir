a5d4eb470c09ede65bf8a4a36a2a32651b848245
TheAfroOfDoom <tehafroofdoom@gmail.com>
2022-07-17 20:13:41 -0400

    Rename leftover `sncov`/`abmseir`s to `abseir`

--- backend/api/wsgi.py
+++ backend/api/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sncov-api project.
+WSGI config for absier-api project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/gunicorn/
